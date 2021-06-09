9438f0d71228e4ec2b20eefe55a00e7a5cbc5414
jianghongbin <jianghongbin@cmcm.com>
2021-06-09 19:37:23 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/redis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/redis.dir/main.c.obj"
+  "libredis.dll.a"
+  "redis.exe"
+  "redis.exe.manifest"
+  "redis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/redis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
